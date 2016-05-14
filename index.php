3b813ff9edaeab2f10b6da97bdf7da3fe5ad6bdf
Victor <VictorShats@users.noreply.github.com>
2016-05-15 01:34:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$outp = '[ {"name":"айнбрух", "desc":"вламання", "color":"blue"},'.
+          '{"name":"аліво", "desc":"та де там", "color":"green"},'.
+          '{"name":"аліяс", "desc":"інакше", "color":"red"},'.
+		  '{"name":"алярм", "desc":"тривога", "color":"yellow"},'.
+		  '{"name":"академус, академік", "desc":"студент академії, університету", "color":"blue"},'.
+          '{"name":"акомодуватися", "desc":"звикати, пристосовуватися", "color":"green"},'.
+		  '{"name":"амант", "desc":"коханець", "color":"red"},'.
+		  '{"name":"андрути", "desc":"вафлі", "color":"yellow"}
+		]';
+	
+echo($outp);
+?>
